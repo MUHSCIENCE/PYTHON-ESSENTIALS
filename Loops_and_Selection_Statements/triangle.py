2c370cbf991e93cda701106c16526777b61f0945
MUHSCIENCE <muhscience08@gmail.com>
2023-06-26 13:58:15 +0300

    Progressing in Python Programming

--- Loops_and_Selection_Statements/triangle.py
+++ Loops_and_Selection_Statements/triangle.py
@@ -1,12 +1,19 @@
 """
 Program: triangle.py
-Author
+Author: ALEX KAMAU
+CHECKS IF A TRIANGLE IS AN EQUILATERAL OR NOT
+1: The inputs are:
+        the sides of the triangle
+2: Compares the sides of the triangle
+        if a=b and b=c
+3: Displays the output
+
 """
 #!/usr/bin/env python
 a = int(input("Enter the side length: "))
 b = int(input("Enter the side length: "))
 c = int(input("Enter the side length: "))
-if a == b and b == c :
+if a == b and b == c:
     print("The triangle is an equilateral triangle")
 else:
     print("The triangle is not an equilateral triangle")
