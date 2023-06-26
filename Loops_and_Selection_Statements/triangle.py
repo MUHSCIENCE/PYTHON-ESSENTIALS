137ef420714f6eee13ce150254e8d28ac0bbe9cb
MUHSCIENCE <muhscience08@gmail.com>
2023-06-26 13:50:25 +0300

    Progressing in Python Programming

--- Loops_and_Selection_Statements/triangle.py
+++ Loops_and_Selection_Statements/triangle.py
@@ -1,8 +1,12 @@
+"""
+Program: triangle.py
+Author
+"""
 #!/usr/bin/env python
-a = int(input("Enter the side length: ")
-b = int(input("Enter the side length: ")
-c = int(input("Enter the side length: ")
+a = int(input("Enter the side length: "))
+b = int(input("Enter the side length: "))
+c = int(input("Enter the side length: "))
 if a == b and b == c :
-    print("The triangle is a equilateral triangle")
+    print("The triangle is an equilateral triangle")
 else:
     print("The triangle is not an equilateral triangle")
